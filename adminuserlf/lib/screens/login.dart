d346ab49c39970b218a3bb4f03396877e1316716
sydammani2003 <sydammani2003@gmail.com>
2025-04-21 16:24:35 +0530

    created staic colours folder

---
+++ adminuserlf/lib/screens/login.dart
@@ -0,0 +1,12 @@
+import 'package:adminuserlf/consts/colors.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage
+({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("data",style: TextStyle(color:Usingcolors.iconscolor ),),),);
+  }
+}
